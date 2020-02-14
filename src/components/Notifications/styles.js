83b2164c665f7266996a8a3d5dd5b3129a5e6e26
Anderson Freitas <mfderson@gmail.com>
2020-02-13 22:05:54 -0300

    Notificações

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -38,6 +38,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${props => (props.visible ? 'block' : 'none')};
 
   /* Evento antes de mostrar NotificationList. Desenha um triângulo */
   &::before {
@@ -74,8 +75,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -83,9 +86,6 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159c1')};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   /* Adiciona uma bolinha laranja na Notification */
@@ -99,6 +99,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
